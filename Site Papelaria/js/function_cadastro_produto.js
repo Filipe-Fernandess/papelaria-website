f44ea44dfc35cbd951ae680a62694b3212f45bd9
Filipe Fernandes <filipefaria1805@gmail.com>
2025-08-15 20:51:03 -0300

    Delete Site Papelaria directory

--- Site Papelaria/js/function_cadastro_produto.js
+++
@@ -1,6 +0,0 @@
-const descricao = document.getElementById("descricao");
-const contador = document.getElementById("contador-descricao");
-
-descricao.addEventListener('input', function() {
-    contador.textContent = `${descricao.value.length}/500`;
-});
