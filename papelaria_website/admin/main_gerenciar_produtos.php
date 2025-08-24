ff59648c32f98cfb69dbccefc85d7ceda8e5c671
Filipe Fernandes <filipefaria1805@gmail.com>
2025-08-24 17:12:50 -0300

    Alteração do backend de gerenciamento de produtos
    Agora o site consulta a categoria do produto no banco de dados.
    Botão "Editar" agora leva para página "editar_produto.php".
    Adicionei uma condição que verifica se o produto está ou não ativo no site.

--- papelaria_website/admin/main_gerenciar_produtos.php
+++ papelaria_website/admin/main_gerenciar_produtos.php
@@ -20,17 +20,17 @@ while ($produto = $resultado_produtos->fetch_assoc()) {
     echo "<tr>";
     echo "<td><img src='" . $produto['imagem_1'] . "' width='100'></td>";
     echo "<td>" . $produto['nome'] . "</td>";
+    echo "<td>" . $produto['categoria'] . "</td>";
     echo "<td>" . $produto['quantidade'] . "</td>";
     echo "<td>" . $produto['preco_atual'] . "</td>";
     echo "<td>" . $produto['percentual_desconto'] . "</td>";
     echo "<td>" . $produto['preco_antigo'] . "</td>";
     echo "<td>" . $produto['descricao'] . "</td>";
-    echo "<td><button>Editar</button></td>";
-    echo "<td>
-            <div class='btn-ativo-inativo'>
-                <button class='ativo' value='true'>Ativo</button>
-                <button class='inativo' value='false'>Inativo</button>
-            </div>
-          </td>";
+    echo "<divclass='cell-editar-produto'><td><a href='editar_produto.php'>Editar</td></a></div>";
+    if ($produto['ativo'] == 1) {
+        echo "<td>Ativo</td>";
+    } else {
+        echo "<td>Inativo</td>";
+    }
     echo "</tr>";
 }
