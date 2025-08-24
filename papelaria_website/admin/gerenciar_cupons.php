7e8a849feecbb8f290991eb4ccb92591eb5bb344
Filipe Fernandes <filipefaria1805@gmail.com>
2025-08-24 17:00:43 -0300

    Alteração de extensão em arquivos do nav-links

--- papelaria_website/admin/gerenciar_cupons.php
+++ papelaria_website/admin/gerenciar_cupons.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="../estilo/style.css">
+    <link rel="stylesheet" href="../estilo/style_admin.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cupons</title>
 </head>
 <body>
-    <header>
+    <header class="head-estilizado">
         <div class = "nav-links">
                 <div><a href="painel.html">Pagina inicial</a></div>
                 <div><a href="gerenciar_produtos.html">Produtos</a></div>
-                <div><a href="gerenciar_cupons.html">Cupons</a></div>
-                <div><a href="banners.html">Banners</a></div>
+                <div><a href="banners.php">Banners</a></div>
                 <div><a href="frases.html">Frases</a></div>
-
         </div>
 </header>
 </body>
+<h1>Gerenciar Cupons</h1>
+
 </html>
